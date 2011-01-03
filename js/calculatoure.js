2c73ea9b516df1247c703333b1fce0ee8129da15
Jussi Kallliokoski <jussi.kalliokoski@gmail.com>
2011-01-03 22:14:11 +0000

    working on a brand new UI.

--- js/calculatoure.js
+++ js/calculatoure.js
@@ -1,82 +1,115 @@
-(function(window){
-	var version = /*# result += version; */;
+(function(global){
+	var	version		= /*# result += version; */,
+		helpData	= [],
+		acceptAsIs	= ['+','-','/','*','%','(',')',',',':','?','^','~','&&','&','||','|','>','>>','>>>','<','<<','>=','<=','!','!=','!==','==','==='],
+		mathFunctions	= ['pow','sin','asin','cos','acos','tan','atan','atan2','pow','floor','ceil','round','abs','sqrt','max','min','log','exp'],
+		customFunctions	= ['whack', 'frac', 'help', 'info', 'x'],
+		results, gui, formulaBox, guiButtons,
+		helpData = [], memHistory = [], memHistoryPos = -1, prev = 0;
+
+	createHelp('sin', Math.sin, 'sin(&alpha;) converts an angle value to an x coordinate.');
+	createHelp('cos', Math.cos, 'cos(&alpha;) converts an angle value to an y coordinate.');
+	createHelp('tan', Math.tan, 'tan(&alpha;) converts an angle value to a line modifier factor (k).');
+	createHelp('asin', Math.asin, 'asin(num) inverse function of sin(&alpha;).');
+	createHelp('acos', Math.acos, 'acos(num) inverse function of cos(&alpha;).');
+	createHelp('atan', Math.atan, 'atan(num) inverse function of tan(&alpha;).');
+	createHelp('atan2', Math.atan2, 'atan2(y, x) inverse function of tan(&alpha;).');
+	createHelp('pow', Math.pow, 'pow(a, b) returns a<sup>b</sup>');
+	createHelp('sqrt', Math.sqrt, 'sqrt(a) returns the square root of a.');
+	createHelp('log', Math.log, 'log(a, b) returns the natural logarithm of a (E-based).');
+	createHelp('sin', Math.exp, 'exp(a) returns the value of E<sup>a</sup>.');
+	createHelp('sin', Math.abs, 'abs(a) returns the absolute value of a.');
+	createHelp('sin', Math.max, 'max(a,b,...) returns the highest value in arguments.');
+	createHelp('sin', Math.min, 'min(a,b,...) returns the lowest value in arguments.');
+	createHelp('sin', Math.floor, 'floor(a) returns a, rounded downwards to the nearest integer.');
+	createHelp('sin', Math.ceil, 'ceil(a) returns a, rounded updwards to the nearest integer.');
+	createHelp('sin', Math.round, 'round(a) returns a, rounded to the nearest integer.');
+	createHelp('sin', frac, 'frac(a) returns the decimal part of a number.');
+	createHelp('sin', whack, 'whack(a) returns all the numbers in a added together and repeated until only one number remains.');
+	whack.toString = function(){return 'function whack() { [native code] }'};
+	frac.toString = function(){return 'function frac() { [native code] }'};
+	help.toString = function(){return 'function help() { [native code] }'};
+	info.toString = function(){info(); return ''};
+	global.calculate = calculate;
+
+	Jin(function(){
+		results = document.getElementById('results');
+		gui = document.getElementById('buttons');
+		formulaBox = document.getElementById('formula');
+		Jin.bind(document, 'keydown', function(e){
+			switch(e.which)
+			{
+				case 13:
+					calculate(new CodeExpression(formulaBox.value, 'JavaScript'));
+					if (memHistoryPos != 0)
+						memHistory.unshift(formulaBox.value);
+					memHistoryPos = -1;
+					formulaBox.value = '';
+					break;
+				case 38:
+					if (memHistoryPos >= memHistory.length - 1)
+						return;
+					formulaBox.value = memHistory[++memHistoryPos];
+					break;
+				case 40:
+					memHistoryPos--;
+					if (memHistoryPos < 0)
+					{
+						memHistoryPos = -1;
+						formulaBox.value = '';
+					}
+					else
+						formulaBox.value = memHistory[memHistoryPos];
+					break;
+				default:
+					/*# if (f.debug) */console.log(e.which);/*# */
+			}
+		});
+		numpadButtons = Jin(document.getElementById('buttons').getElementsByTagName('button'));
+		numpadButtons.bind('click', function(){ fixData(this); });
+		Jin.bind(document.getElementById('calculate'), 'click', function(){
+			calculate(new CodeExpression(formulaBox.value, 'JavaScript'));
+			memHistory.unshift(formulaBox.value);
+			memHistoryPos = -1;
+			formulaBox.value = '';
+		});
+		//Jin.bind(document.getElementById('header'), 'click', function(){ info(); });
+	});
+
+	// Custom functions
+
 	function whack(num)
 	{
 		return (num-1) % 9 + 1;
 	}
+
 	function frac(num)
 	{
 		return num - Math.floor(num);
 	}
+
 	function help(about)
 	{
-		switch(about)
-		{
-			case Math.sin:
-				addLine('help:<br />sin(&alpha;) converts an angle value to an x coordinate.');
-				break;
-			case Math.cos:
-				addLine('help:<br />cos(&alpha;) converts an angle value to an y coordinate.');
-				break;
-			case Math.tan:
-				addLine('help:<br />tan(&alpha;) converts an angle value to a line modifier factor (k).');
-				break;
-			case Math.asin:
-				addLine('help:<br />asin(num) inverse function of sin(&alpha;).');
-				break;
-			case Math.acos:
-				addLine('help:<br />acos(num) inverse function of cos(&alpha;).');
-				break;
-			case Math.atan:
-				addLine('help:<br />atan(num) inverse function of tan(&alpha;).');
-				break;
-			case Math.atan2:
-				addLine('help:<br />atan2(y, x) inverse function of tan(&alpha;).');
-				break;
-			case Math.pow:
-				addLine('help:<br />pow(a, b) returns a<sup>b</sup>');
-				break;
-			case Math.sqrt:
-				addLine('help:<br />sqrt(a) returns the square root of a.');
-				break;
-			case Math.log:
-				addLine('help:<br />log(a, b) returns the natural logarithm of a (E-based).');
-				break;
-			case Math.exp:
-				addLine('help:<br />exp(a) returns the value of E<sup>a</sup>.');
-				break;
-			case Math.abs:
-				addLine('help:<br />abs(a) returns the absolute value of a.');
-				break;
-			case Math.max:
-				addLine('help:<br />max(a,b,...) returns the highest value in arguments.');
-				break;
-			case Math.min:
-				addLine('help:<br />min(a,b,...) returns the lowest value in arguments.');
-				break;
-			case Math.floor:
-				addLine('help:<br />floor(a) returns a, rounded downwards to the nearest integer.');
-				break;
-			case Math.ceil:
-				addLine('help:<br />ceil(a) returns a, rounded updwards to the nearest integer.');
-				break;
-			case Math.round:
-				addLine('help:<br />round(a) returns a, rounded to the nearest integer.');
-				break;
-			case frac:
-				addLine('help:<br />frac(a) returns the decimal part of a number.');
-				break;
-			case whack:
-				addLine('help:<br />whack(a) returns all the numbers in a added together and repeated until only one number remains.');
-				break;
-			default:
-				addLine('help:<br />type help(&lt;function name&gt;) to get help with that specific function.');
-		}
-		return '!HELP';
+		for (var i=0; i<helpData.length; i++)
+			if (helpData[i].f === about)
+			{
+				addLine('help:<br/>' + helpData[i].h);
+				return '$';
+			}
+		addLine('help:<br />type help(&lt;function name&gt;) to get help with that specific function.');
+		return '$';
 	}
+
+	// Other
+
+	function createHelp(name, func, help)
+	{
+		helpData.push({n: name, f: func, h: help});
+	}
+
 	function info()
 	{
-		var inf = document.getElementById('info');
+		/*var inf = document.getElementById('info');
 		if (inf)
 		{
 			Jin.removeClass(inf, 'active');
@@ -89,50 +122,51 @@
 		document.body.appendChild(inf);
 		setTimeout(function(){Jin.addClass(inf, 'active');}, 1);
 		Jin.bind(inf, 'click', function(){ info(); });
-		return 'Showing info...';
-	}
-	whack.toString = function(){return 'function whack() { [native code] }'};
-	frac.toString = function(){return 'function frac() { [native code] }'};
-	help.toString = function(){return 'function help() { [native code] }'};
-	info.toString = function(){info(); return ''};
-	window.calculate = calculate;
-	var prev = 0;
-
-	function writeDown(mexpr)
-	{
-		for(var i=0, s=[]; i<mexpr.length; i++)
-		{
-			if (mexpr[i].content.toLowerCase() == 'pi')
-				s.push('pi');
-			else if(mexpr[i].type != 'Whitespace')
-				s.push(mexpr[i].content.toLowerCase());
-		}
-		return s.join(' ');
+		return 'Showing info...';*/
 	}
 
-	function calculate(mexpr)
+	function fixData(button)
 	{
-		try{
-			var num = prev, x = num, expr = createExpr(mexpr);
-			eval('num='+expr);
-			if (num === '!HELP')
-				return;
-			prev = num;
-			addLine(writeDown(mexpr), 'expression');
-			addLine(num, 'result');
-		}
-		catch(e)
-		{
-/*# if(f.debug) */
-			console.log(expr);
+		var	str	= button.innerHTML
+				.replace(/&lt;/g, '<')
+				.replace(/&gt;/g, '>')
+				.replace(/&amp;/g, '&'),
+			cl	= button.className;
+		if(str == 'clear')
+			return formulaBox.value = '';
+		if (cl !== 'op' && cl !== 'num' && str.length > 2)
+			str += '(';
+		pushStr(str);
+	}
+
+	function addLine(str, type)
+	{
+		var div = document.createElement('div');
+		div.innerHTML = str;
+		Jin.addClass(div, type);
+		results.appendChild(div);
+		results.scrollTop = results.scrollHeight;
+	}
+
+	function pushStr(str)
+	{
+		var length = formulaBox.length,
+		start = formulaBox.selectionStart,
+		end = formulaBox.selectionEnd,
+		sel = formulaBox.value.substr(start, end);
+		formulaBox.value = formulaBox.value.substr(0, start) + str; + formulaBox.value.substr(end);
+/*# if (!f.mobile) */
+		formulaBox.focus();
 /*# */
-			addLine(e, 'error');
-		}
 	}
 
-	var acceptAsIs = ['+','-','/','*','%','(',')',',',':','?','^','~','&&','&','||','|','>','>>','>>>','<','<<','>=','<=','!','!=','!==','==','==='],
-	mathFunctions = ['pow','sin','asin','cos','acos','tan','atan','atan2','pow','floor','ceil','round','abs','sqrt','max','min','log','exp'],
-	customFunctions = ['whack', 'frac', 'help', 'info', 'x'];
+	function isIn(needle, haystack)
+	{
+		for (var i=0, l=haystack.length; i<l; i++)
+			if (haystack[i] === needle)
+				return true;
+		return false;
+	}
 
 	function createExpr(mexpr)
 	{
@@ -171,94 +205,35 @@
 		return expr;
 	}
 
-	var results, formulabox, gui, typebox, memhist = [], numpadButtons;
-	memhist.position = -1;
-
-	Jin(function(){
-		results = document.getElementById('results');
-		formulabox = document.getElementById('formulabox');
-		gui = document.getElementById('gui');
-		typebox = document.getElementById('typebox');
-		Jin.bind(document, 'keydown', function(e){
-			switch(e.which)
-			{
-				case 13:
-					calculate(new CodeExpression(typebox.value, 'JavaScript'));
-					if (memhist.position != 0)
-						memhist.unshift(typebox.value);
-					memhist.position = -1;
-					typebox.value = '';
-					break;
-				case 38:
-					if (memhist.position >= memhist.length - 1)
-						return;
-					typebox.value = memhist[++memhist.position];
-					break;
-				case 40:
-					memhist.position--;
-					if (memhist.position < 0)
-					{
-						memhist.position = -1;
-						typebox.value = '';
-					}
-					else
-						typebox.value = memhist[memhist.position];
-					break;
-				default:
-					/*# if (f.debug) */console.log(e.which);/*# */
-			}
-		});
-		numpadButtons = Jin(document.getElementById('numpad').getElementsByTagName('button'));
-		numpadButtons.bind('click', function(){ fixData(this); });
-		Jin.bind(document.getElementById('calculate'), 'click', function(){
-			calculate(new CodeExpression(typebox.value, 'JavaScript'));
-			memhist.unshift(typebox.value);
-			memhist.position = -1;
-			typebox.value = '';
-		});
-		Jin.bind(document.getElementById('header'), 'click', function(){ info(); });
-	});
-
-	function fixData(button)
+	function writeDown(mexpr)
 	{
-		var	str	= button.innerHTML
-				.replace(/&lt;/g, '<')
-				.replace(/&gt;/g, '>')
-				.replace(/&amp;/g, '&'),
-			cl	= button.className;
-		if(str == 'clear')
-			return typebox.value = '';
-		if (cl !== 'op' && cl !== 'num' && str.length > 2)
-			str += '(';
-		pushStr(str);
+		for(var i=0, s=[]; i<mexpr.length; i++)
+		{
+			if (mexpr[i].content.toLowerCase() == 'pi')
+				s.push('pi');
+			else if(mexpr[i].type != 'Whitespace')
+				s.push(mexpr[i].content.toLowerCase());
+		}
+		return s.join(' ');
 	}
 
-	function addLine(str, type)
+	function calculate(mexpr)
 	{
-		var div = document.createElement('div');
-		div.innerHTML = str;
-		Jin.addClass(div, type);
-		results.appendChild(div);
-		results.scrollTop = results.scrollHeight;
-	}
-
-	function pushStr(str)
-	{
-		var length = typebox.length,
-		start = typebox.selectionStart,
-		end = typebox.selectionEnd,
-		sel = typebox.value.substr(start, end);
-		typebox.value = typebox.value.substr(0, start) + str; + typebox.value.substr(end);
-/*# if (!f.mobile) */
-		typebox.focus();
+		try{
+			var num = prev, x = num, expr = createExpr(mexpr);
+			eval('num='+expr);
+			if (num === '!HELP')
+				return;
+			prev = num;
+			addLine(writeDown(mexpr), 'expression');
+			addLine(num, 'result');
+		}
+		catch(e)
+		{
+/*# if(f.debug) */
+			console.log(expr);
 /*# */
+			addLine(e, 'error');
+		}
 	}
-
-	function isIn(needle, haystack)
-	{
-		for (var i=0, l=haystack.length; i<l; i++)
-			if (haystack[i] === needle)
-				return true;
-		return false;
-	}
-})(window);
+})(this);
