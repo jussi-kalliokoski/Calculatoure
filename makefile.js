0645c25953bd395481e8478436ec06ccb5110047
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-03-02 14:36:11 +0200

    Updated makefile and chrome ext manifest

--- makefile.js
+++ makefile.js
@@ -1,15 +1,15 @@
 var version = 0.998;
 
 function webapp(){
-	shell('cd webapp; makejs -v ' + version + ' ' + makejs.rawFlags.join(' '));
+	shell('cd webapp; makejs -v ' + version + ' --passed-flags ' + makejs.rawFlags.join(' '));
 }
 
 function extensions(){
-	shell('cd extension; makejs -v ' + version + ' ' + makejs.rawFlags.join(' '));
+	shell('cd extension; makejs -v ' + version + ' --passed-flags ' + makejs.rawFlags.join(' '));
 }
 
 function api(){
-	shell('cd api; makejs -v ' + version + ' ' + makejs.rawFlags.join(' '));
+	shell('cd api; makejs -v ' + version + ' --passed-flags ' + makejs.rawFlags.join(' '));
 }
 
 function all(){
