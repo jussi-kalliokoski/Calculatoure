95ef4644a40a39d79567089b3aee5e9477768d11
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-03-02 13:54:23 +0200

    Updated makefiles.

--- webapp/html/makefile.js
+++ webapp/html/makefile.js
@@ -1,29 +1,50 @@
-echo ('Generating HTML files...');
 import('conditional.js');
+
 var flags = [];
-function all()
-{
-	var html = open('calculatoure.html').split('<!-- BUTTONS -->'),
-	layout = open('buttonlayout.xml').split('\n\n'),
-	layoutCaptions = [], layoutTypes = [], n, i, out = [];
+
+echo ('Generating HTML files...');
+
+function _compile(name){
+	var	html		= open('calculatoure.html').split('<!-- BUTTONS -->'),
+		layout		= open('buttonlayout.xml').split('\n\n'),
+		layoutCaptions	= [],
+		layoutTypes	= [],
+		n, i, out	= [];
+
 	n = layout[0].split('\n');
-	for (i=0; i<n.length; i++)
+
+	for (i=0; i<n.length; i++){
 		layoutCaptions[i] = n[i].split('\t');
+	}
 	n = layout[1].split('\n');
-	for (i=0; i<n.length; i++)
+	for (i=0; i<n.length; i++){
 		layoutTypes[i] = n[i].split('\t');
-	for (i=0; i<layoutCaptions.length; i++)
-	{
+	}
+	for (i=0; i<layoutCaptions.length; i++){
 		layout = [];
-		for (n=0; n<layoutCaptions[i].length; n++)
+		for (n=0; n<layoutCaptions[i].length; n++){
 			layout[n] = '<button class="' + layoutTypes[i][n] + '">' + layoutCaptions[i][n] + '</button>';
+		}
 		out.push(layout.join(''));
 	}
 	var buttons = out.join('<br />');
 	save('../temp/index.html', html[0] + buttons + html[1]);
 }
 
-function gzip()
-{
-	flags.push('gzip');
+function common(){
+	_compile('common');
+}
+
+function webkit(){
+	_compile('webkit');
+}
+
+function mobile(){
+	_compile('mobile');
+}
+
+function all(){
+	common();
+	webkit();
+	mobile();
 }
--- webapp/makefile.js
+++ webapp/makefile.js
@@ -16,6 +16,7 @@ function remove(needle, haystack){
 			haystack.splice(i--, 1);
 		}
 	}
+	return haystack;
 }
 
 import('conditional.js');
@@ -35,20 +36,19 @@ console.log('Making webapp v. ' + version + ' with flags ' + globalFlags.join(',
 function _compile(flags){
 	console.log('Compiling...');
 	console.log('Checking if API is up to date...');
-	shell('mkdir temp');
 	shell('cd ../api/; if [ -s calculatoure.api.js ]; then echo "Up to date, moving on..."; else echo "Not up to date, making..."; makejs -v ' + version + '; fi');
 	shell('cp ../api/calculatoure.api.js temp/');
 	console.log('Making UI...');
 	var data	= open('../deps/jin.js/js/jin.js') + 
-			Conditional.parseJS(open('js/calculatoure.ui.js')(), flags);
+			Conditional.parseJS(open('js/calculatoure.ui.js'), flags)();
 	save('temp/calculatoure.ui.js', data);
-	data		= Conditional.parseJS(open('js/calculatoure.css')(), flags);
+	data		= Conditional.parseJS(open('css/calculatoure.css'), flags)();
 	save('temp/calculatoure.css', data);
 }
 
 function _compress(name, flags){
 	flags = flags || globalFlags;
-	if (isIn('compress', flags){
+	if (isIn('compress', flags)){
 		console.log('Compressing...');
 		shell('yui-compressor temp/calculatoure.api.js -o out/' + name + '/calculatoure.api.js');
 		shell('yui-compressor temp/calculatoure.ui.js -o out/' + name + '/calculatoure.ui.js');
@@ -67,11 +67,12 @@ function _compress(name, flags){
 function _makeHTML(name, flags){
 	flags = flags || globalFlags;
 	shell('cd html/; makejs ' + name + ' -v ' + version);
-	shell('cp temp/index.html out/ ' + name + '/');
+	shell('cp temp/index.html out/' + name + '/');
 }
 
 function _copyImages(name, flags){
 	flags = flags || globalFlags;
+	console.log('Copying images...');
 	shell('cp ../img/favicon.png out/' + name);
 	shell('cp ../img/dark.png out/' + name);
 	shell('cp ../img/light.png out/' + name);
@@ -82,7 +83,7 @@ function _start(name, flags){
 	console.log('Making ' + name + ' version...');
 	var	flags		= [name].concat(globalFlags);
 	console.log('Using flags ' + flags.join(', '));
-	shell('mkdir out/' + name + '/ -p');
+	shell('mkdir out/' + name + '/ temp -p');
 	shell('rm out/' + name + '/* -f');
 	_compile(flags);
 	_compress(name, flags);
@@ -92,7 +93,7 @@ function _start(name, flags){
 }
 
 function common(){
-	var	flags		= _start('webkit');
+	var	flags		= _start('common');
 }
 
 function webkit(){
