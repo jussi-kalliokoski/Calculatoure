35ae77ca84394d7b45cd5ee5d1d648308fb1784f
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-03-02 12:53:56 +0200

    Updated webapp makefile

--- webapp/makefile.js
+++ webapp/makefile.js
@@ -1,162 +1,122 @@
-Array.prototype.isIn = function(needle)
+function isIn(needle, haystack)
 {
-	for (var i=0, l=this.length; i<l; i++)
-		if (this[i] === needle)
+	var i, l = haystack.length;
+	for (i=0; i<l; i++)
+		if (haystack[i] === needle)
 			return true;
 	return false;
 }
 
-Array.prototype.remove = function(needle){
-	for (var i=0; i<this.length; i++){
-		if (this[i] === needle){
-			this.splice(i--, 1);
+function remove(needle, haystack){
+	var i;
+	for (i=0; i<haystack.length; i++){
+		if (haystack[i] === needle){
+			haystack.splice(i--, 1);
 		}
 	}
 }
 
-var version = 0.998,
-globalFlags = ["compress", "gzip"],
-flagsTampered = false;
-echo ("Building CALCULATOURE (v. " + version + ")");
-shell("mkdir out/common out/mobile out/webkit temp -p");
+import('conditional.js');
 
-import("conditional.js");
+var	version		= makejs.argumentedFlags['-v'] || makejs.argumentedFlags['--version'] || 0.1,
+	globalFlags	= [];
 
-function all()
-{
-	flagsTampered = false;
+if (!isIn('--no-compress', makejs.flags)){
+	globalFlags.push('compress');
+	if (!isIn('--no-gzip', makejs.flags)){
+		globalFlags.push('gzip');
+	}
+}
+
+console.log('Making webapp v. ' + version + ' with flags ' + globalFlags.join(', '));
+
+function _compile(flags){
+	console.log('Compiling...');
+	console.log('Checking if API is up to date...');
+	shell('cd ../api/; if [ -s calculatoure.api.js ]; then; echo "Not up to date, making..."; makejs -v ' + version + '; else; echo "Up to date, moving on..."; fi');
+	shell('cp ../api/calculatoure.api.js temp/');
+	console.log('Making UI...');
+	var data	= open('../deps/jin.js/js/jin.js') + 
+			Conditional.parseJS(open('js/calculatoure.ui.js'), flags);
+	save('temp/calculatoure.ui.js', data);
+	data		= Conditional.parseJS(open('js/calculatoure.ui.js'), flags);
+	save('temp/calculatoure.css', data);
+}
+
+function _compress(name, flags){
+	flags = flags || globalFlags;
+	if (isIn('compress', flags){
+		console.log('Compressing...');
+		shell('yui-compressor temp/calculatoure.api.js -o out/' + name + '/calculatoure.api.js');
+		shell('yui-compressor temp/calculatoure.ui.js -o out/' + name + '/calculatoure.ui.js');
+		shell('yui-compressor temp/calculatoure.css -o out/' + name + '/calculatoure.css');
+		if (isIn('gzip', flags)){
+			shell('cd out/' + name + '/; gzip calculatoure.api.js -c -f > calculatoure.api.jgz');
+			shell('cd out/' + name + '/; gzip calculatoure.ui.js -c -f > calculatoure.ui.jgz');
+			shell('cd out/' + name + '/; gzip calculatoure.css -c -f > calculatoure.cgz');
+			shell('cp misc/htaccess_for_jgz out/' + name + '/.htaccess');
+		}
+	} else {
+		shell('cp temp/* out/' + name + '/');
+	}
+}
+
+function _makeHTML(name, flags){
+	flags = flags || globalFlags;
+	shell('cd html/; makejs ' + name + ' -v ' + version);
+	shell('cp temp/index.html out/ ' + name + '/');
+}
+
+function _copyImages(name, flags){
+	flags = flags || globalFlags;
+	shell('cp ../img/favicon.png out/' + name);
+	shell('cp ../img/dark.png out/' + name);
+	shell('cp ../img/light.png out/' + name);
+}
+
+function _start(name, flags){
+	flags = flags || globalFlags;
+	console.log('Making ' + name + ' version...');
+	var	flags		= [name].concat(globalFlags);
+	console.log('Using flags ' + flags.join(', '));
+	shell('mkdir out/' + name + '/ -p');
+	shell('rm out/' + name + '/* -f');
+	_compile(flags);
+	_compress(name, flags);
+	_makeHTML(name, flags);
+	_copyImages(name, flags);
+	return flags;
+}
+
+function common(){
+	var	flags		= _start('webkit');
+}
+
+function webkit(){
+	var	flags		= [].concat(globalFlags),
+		manifest;
+
+	remove('compress', flags);
+	flags = _start('webkit', flags);
+		
+	manifest		= open('misc/manifest.json');
+
+	manifest		= Conditional.ParseJS(manifest, flags);
+	save('out/webkit/manifest.json', manifest);
+	shell('cp ../img/calculatoure.png out/webkit/icon_128.png; cp ../img/favicon.png out/webkit/icon_16.png');
+	shell('cd out/webkit/; zip calculatoure *');
+}
+
+function mobile(){
+	var	flags		= _start('mobile');
+}
+
+function all(){
 	common();
 	webkit();
-//	mobile();
+	mobile();
 }
 
-function common()
-{
-	flagsTampered = false;
-	echo("Making common version...\n");
-	var flags = ['common'].concat(globalFlags);
-	echo("Using flags " + flags);
-	compile(flags);
-	compress("common");
-	makeHTML("common");
-	copyImages("common");
-}
-
-function webkit()
-{
-	flagsTampered = false;
-	echo("Making webkit version...\n");
-	shell("rm out/webkit/* -f");
-	var flags = ['webkit'].concat(globalFlags),
-	manifest = open("misc/manifest.json");
-	echo("Using flags " + flags);
-	manifest = Conditional.parseJS(manifest, flags)();
-	save("out/webkit/manifest.json", manifest);
-	compile(flags);
-	compress("webkit", []);
-	makeHTML("webkit");
-	copyImages("webkit");
-	shell("cp img/calculatoure.png out/webkit/icon_128.png; cp img/favicon.png out/webkit/icon_16.png");
-	shell("cd out/webkit/; zip calculatoure *");
-}
-
-function mobile()
-{
-	flagsTampered = false
-	echo("Making mobile version...\n");
-	var flags = ['mobile'].concat(globalFlags);
-	echo("Using flags " + flags);
-	compile(flags);
-	compress("mobile");
-	makeHTML("mobile");
-	copyImages("mobile");
-}
-
-function clean()
-{
-	flagsTampered = false;
-	echo ("Cleaning up...");
-	shell("rm temp/* -f");
-	shell("rm out/common/* -f");
-	shell("rm out/webkit/* -f");
-	shell("rm out/mobile/* -f");
-	shell("cd deps/codeexpression-js/;makejs clean");
-}
-
-function compile(flags)
-{
-	echo ("Compiling...");
-	shell("cd deps/codeexpression-js/;makejs");
-	var data = open("deps/codeexpression-js/CodeExpression.min.js") +
-		Conditional.parseJS(open("js/calculatoure.api.js"), flags)();
-	if (flags.isIn("unitTests")){
-		data += open("js/unit-tests.js");
-	}
-	save("temp/calculatoure.api.js", data);
-	data = open("js/jin.js") +
-		Conditional.parseJS(open("js/calculatoure.ui.js"), flags)();
-	save("temp/calculatoure.ui.js", data);
-	data = Conditional.parseJS(open("css/calculatoure.css"), flags)();
-	save("temp/calculatoure.css", data);
-}
-
-function compress(directory, flags)
-{
-	flags = flags || globalFlags;
-	echo ("Compressing...");
-	shell("yui-compressor temp/calculatoure.api.js -o out/" + directory + "/calculatoure.api.js");
-	shell("yui-compressor temp/calculatoure.ui.js -o out/" + directory + "/calculatoure.ui.js");
-	shell("yui-compressor temp/calculatoure.css -o out/" + directory + "/calculatoure.css");
-	//shell("cp misc/manifest.php out/" + directory + "/");
-	if (flags.isIn("gzip"))
-	{
-		shell("cd out/" + directory + "; gzip calculatoure.api.js -c -f > calculatoure.api.jgz");
-		shell("cd out/" + directory + "; gzip calculatoure.ui.js -c -f > calculatoure.ui.jgz");
-		shell("cp misc/htaccess_for_jgz out/" + directory + "/.htaccess");
-	}
-}
-
-function makeHTML(directory)
-{
-	shell("cd html/ && ls && makejs " + (globalFlags.isIn("gzip") ? "gzip " : "") + "all");
-	shell("cp temp/index.html out/" + directory);
-}
-
-function copyImages(directory)
-{
-	shell("cp img/favicon.png out/" + directory);
-	shell("cp img/dark.png out/" + directory);
-	shell("cp img/light.png out/" + directory);
-}
-
-function debug()
-{
-	flagsTampered = true;
-	globalFlags.push('debug');
-}
-
-function noGzip()
-{
-	flagsTampered = true;
-	globalFlags.remove('gzip');
-}
-
-function unitTests()
-{
-	flagsTampered = true;
-	globalFlags.push('unitTests');
-}
-
-function noCompress()
-{
-	flagsTampered = true;
-	globalFlags.remove('compress');
-}
-
-function onfinish()
-{
-	if (flagsTampered){
-		all();
-	}
-	echo("Done!");
+function onfinish(){
+	console.log('Done!');
 }
