8fd7fed5e10208c6752d67d4985f247b5ce40ea8
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-03-02 14:26:13 +0200

    Updated extensions

--- extension/makefile.js
+++ extension/makefile.js
@@ -1,3 +1,18 @@
+console.log('Making extensions...');
+
+import('conditional.js');
+
+var	version		= makejs.argumentedFlags['-v'] || makejs.argumentedFlags['--version'] || 0.1;
+
+function chrome(){
+	console.log('Making chrome extension...');
+	shell('mkdir out/chrome -p');
+	shell('cp html/popup.html out/chrome/');
+	shell('cp ../img/favicon.png out/chrome/icon.png');
+	var data	= Conditional.parseJS(open('misc/manifest.json'), [])();
+	save('out/chrome/manifest.json', data);
+}
+
 function all(){
-	console.log('Nothing to do here yet.');
+	chrome();
 }
