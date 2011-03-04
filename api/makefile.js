53f140afff22d7c75a3426afae4d6a0b8ef6ab62
jussi-kalliokoski <jussi.kalliokoski@gmail.com>
2011-03-04 08:45:55 +0200

    Updated makefile for API.

--- api/makefile.js
+++ api/makefile.js
@@ -1,22 +1,38 @@
-var	version		= makejs.argumentedFlags['-v'] || makejs.argumentedFlags['--version'] || 0.1;
+var	version		= makejs.argumentedFlags['-v'] || makejs.argumentedFlags['--version'] || 0.1,
+	_force		= makejs.flags.indexOf('-f') !== -1 || makejs.flags.indexOf('--force') !== -1;
 
 import('conditional.js');
-
-console.log('Making Calculatoure API...');
+import('build.js');
 
 function all(){
-	console.log('Checking if CodeExpression.js is up to date...');
-	shell('cd deps/CodeExpression.js/; if [ -s CodeExpression.full.js ]; then echo "Up to date, moving on..."; else echo "Not up to date, making..."; makejs --just JavaScript; fi');
-	var data	= open('deps/CodeExpression.js/CodeExpression.full.js') +
-			Conditional.parseJS(open('js/calculatoure.api.js'), [])();
+	console.log('Making Calculatoure API...');
+	shell('cd deps/CodeExpression.js/; makejs --just JavaScript');
+	
+	var	sameFlags = false,
+		data;
+
+	try{
+		sameFlags = open('.makejs.config') == makejs.rawFlags.join(' ');
+	} catch(e){}
+
+	if (sameFlags){
+		if (!_force && all.isUpToDate()){
+			console.log('Already up to date!');
+			return;
+		}
+	} else {
+		save('.makejs.config', makejs.rawFlags.join(' '));
+	}
+
+	data	= open('deps/CodeExpression.js/CodeExpression.full.js') +
+		Conditional.parseJS(open('js/calculatoure.api.js'), [])();
 	save('calculatoure.api.js', data);
 	shell('yui-compressor calculatoure.api.js -o calculatoure.api.min.js');
+	console.log('Done!');
 }
 
 function test(){
 	shell('node unit-tests/unit-test.js');
 }
 
-function onfinish(){
-	console.log('Done!');
-}
+Build.createBuild(all, ['calculatoure.api.js'], ['deps/CodeExpression.js/CodeExpression.full.js', 'js/calculatoure.api.js']);
