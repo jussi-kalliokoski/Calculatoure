43e3ed22c4756a6288eab983547763a5894c14e3
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2011-04-01 10:39:07 +0300

    Added more versatile makefile for the CLI.

--- bash/makefile.js
+++ bash/makefile.js
@@ -12,6 +12,19 @@ function all(){
 	shell('cd ../api/; makejs commonjs --passFlags ' + makejs.rawFlags + ' --no-shims');
 	console.log('Compiling...');
 	var data = Conditional.parseJS(open('calculatoure-cli'), [])();
-	save(_bindir + _binname, data);
-	shell('chmod ugo+x ' + _bindir + _binname);
+	shell('mkdir bin/ -p');
+	save('bin/' + _binname, data);
+	shell('chmod ugo+x bin/' + _binname);
+}
+
+function install(){
+	shell('mv bin/' + _binname + ' ' + _bindir);
+}
+
+function clean(){
+	shell('rm bin/ -p');
+}
+
+function uninstall(){
+	shell('rm ' + _bindir + _binname);
 }
